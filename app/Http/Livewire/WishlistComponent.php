8bf82a825f0bb8d13227d8a16730a5755ba42811
SelimReza1 <selimreza9920@gmail.com>
2021-08-25 20:55:49 +0600

    move product from wishlist to cart and quantity on product details page

--- app/Http/Livewire/WishlistComponent.php
+++ app/Http/Livewire/WishlistComponent.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire;
 
+
 use Livewire\Component;
 use Cart;
 class WishlistComponent extends Component
@@ -16,6 +17,16 @@ class WishlistComponent extends Component
             }
         }
     }
+
+    public function moveProductFromWishlistToCart($rowId)
+    {
+        $item = Cart::instance('wishlist')->get($rowId);
+        Cart::instance('wishlist')->remove($rowId);
+        Cart::instance('cart')->add($item->id,$item->name,1,$item->price)->associate('App\Models\Product');
+        $this->emitTo('wishlist-count-component','refreshComponent');
+        $this->emitTo('cart-count-component','refreshComponent');
+    }
+
     public function render()
     {
         return view('livewire.wishlist-component')->layout('layouts.base');
