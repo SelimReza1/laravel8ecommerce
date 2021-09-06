d28cd6027c7537bb7a2f97694172b3b4f0cd5a6c
SelimReza1 <selimreza9920@gmail.com>
2021-09-06 20:07:13 +0600

    Admin Create Model, Migration and Relationship for Checkout

---
+++ app/Models/Order.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    protected $table = "orders";
+
+    public function users(){
+        $this->belongsTo(User::class);
+    }
+
+    public function orderItems(){
+        $this->hasMany(OrderItem::class);
+    }
+
+    public function shipping(){
+        $this->hasOne(Shipping::class);
+    }
+
+    public function transaction(){
+        $this->hasOne(Transaction::class);
+    }
+
+
+}
