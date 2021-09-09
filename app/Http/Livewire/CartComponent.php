345dc64f3b11d057ee15b835e81823ff2845cdc3
SelimReza1 <selimreza9920@gmail.com>
2021-09-09 16:34:48 +0600

    checkout with cash on delivery

--- app/Http/Livewire/CartComponent.php
+++ app/Http/Livewire/CartComponent.php
@@ -91,6 +91,11 @@ class CartComponent extends Component
     }
 
     public function setAmountForCheckout(){
+        if (!Cart::instance('cart')->count() > 0)
+        {
+            session()->forget('checkout');
+        return;
+        }
         if (session()->has('coupon'))
         {
             session()->put('checkout',[
